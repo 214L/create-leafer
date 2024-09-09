288847cda79d031296acb0117b95e1d71300638d
214L <Aierc_W@hotmail.com>
2024-09-09 22:32:58 +0800

    feat : add init command
    @description : add entrance for init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,8 +1,33 @@
+import path from 'node:path'
 import { Command } from 'commander'
-// import { parseArgs } from 'node:util'
-
-
+import { z } from 'zod'
+import { existsSync } from 'fs-extra'
+import { red } from 'kolorist'
+const initOptionsSchema = z.object({
+  cwd: z.string(),
+  yes: z.boolean(),
+  defaults: z.boolean()
+})
 export const init = new Command()
   .name('init')
-  //
-  .action(async opts => {})
+  .description('initialize your leafer project and install dependencies')
+  .option('-y, --yes', 'skip confirmation prompt.', false)
+  .option('-d, --defaults,', 'use default configuration.', false)
+  .option(
+    '-c, --cwd <cwd>',
+    'the working directory. defaults to the current directory.',
+    process.cwd()
+  )
+  .action(async opts => {
+    try {
+      const options = initOptionsSchema.parse(opts)
+      const cwd = path.resolve(options.cwd)
+
+      if (!existsSync(cwd)) {
+        red(`The path ${cwd} does not exist. Please try again.`)
+        process.exit(1)
+      }
+    } catch (error) {
+      // handleError(error)
+    }
+  })
