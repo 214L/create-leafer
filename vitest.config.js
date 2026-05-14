8dc92fa2487f2312e87b68be027dacba2845d4b9
214L <Aierc_W@hotmail.com>
2026-05-14 11:16:43 +0800

    test(vitest): 限定测试匹配范围

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,9 @@
-// vitest.config.ts
+// vitest.config.js
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['__test__/**/*.test.ts'],
     exclude: [
       'node_modules',
       'template/*/**', 
